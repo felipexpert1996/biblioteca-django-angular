d19520bc5808b633798c1c60c2aaca36f6516d2a
Felipe <gfas@cesar.org.br>
2021-01-01 00:47:17 -0300

    close #14

--- backend/apps/accounts/tests.py
+++ backend/apps/accounts/tests.py
@@ -3,6 +3,8 @@ from faker import Faker
 from django.urls import reverse
 from rest_framework import status
 from faker.providers import internet, misc
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 
 class AccountsTest(APITestCase):
@@ -25,3 +27,21 @@ class AccountsTest(APITestCase):
 
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_login(self):
+        url = reverse('login')
+        fake = Faker()
+        fake.add_provider(internet)
+        fake.add_provider(misc)
+        name = fake.user_name()
+        email = fake.email()
+        password = fake.password(length=8)
+        user = User.objects.create_user(name, email, password)
+        data = {
+            'email': email,
+            'password': password
+        }
+        response = self.client.post(url, data, format='json')
+        token = Token.objects.get(user=user)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(f'{response.data["key"]}', f'{token}')
